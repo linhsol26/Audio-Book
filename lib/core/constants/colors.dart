f7914703f44f28988eb50efc8cd4d8bf491e31d9
truongan99 <81751920+truongan99@users.noreply.github.com>
2022-01-19 11:21:16 +0700

    commit color common

---
+++ lib/core/constants/colors.dart
@@ -0,0 +1,43 @@
+import 'package:flutter/material.dart';
+
+class AppColor {
+  //primary
+  static const primary_5 = Color(0xffF3F1FE);
+  static const primary_10 = Color(0xffDDD7FC);
+  static const primary_20 = Color(0xffBBB1FA);
+  static const primary_30 = Color(0xff9487F1);
+  static const primary_40 = Color(0xff7466E3);
+  static const primary_50 = Color(0xff4838D1);
+  static const primary_60 = Color(0xff3528B3);
+  static const primary_70 = Color(0xff261C96);
+  static const primary_80 = Color(0xff191179);
+  static const primary_90 = Color(0xff100A64);
+  static const primary_100 = Color(0xff090638);
+  //accent
+  static const accent_5 = Color(0xffFFFAF5);
+  static const accent_10 = Color(0xffFEEEDD);
+  static const accent_20 = Color(0xffFED9BB);
+  static const accent_30 = Color(0xffFCBE99);
+  static const accent_40 = Color(0xffFAA47F);
+  static const accent_50 = Color(0xffF77A55);
+  static const accent_60 = Color(0xffD4553E);
+  static const accent_70 = Color(0xffB1362A);
+  static const accent_80 = Color(0xff8F1C1B);
+  static const accent_90 = Color(0xff761016);
+  static const accent_100 = Color(0xff480A0D);
+  //neutral
+  static const neutral_5 = Color(0xffF5F5FA);
+  static const neutral_10 = Color(0xffEBEBF5);
+  static const neutral_20 = Color(0xffD5D5E3);
+  static const neutral_30 = Color(0xffB8B8C7);
+  static const neutral_40 = Color(0xffB8B8C7);
+  static const neutral_50 = Color(0xff9292A2);
+  static const neutral_60 = Color(0xff6A6A8B);
+  static const neutral_70 = Color(0xff494974);
+  static const neutral_80 = Color(0xff2E2E5D);
+  static const neutral_90 = Color(0xff1C1C4D);
+  static const neutral_100 = Color(0xff0F0F29);
+  //black and white
+  static const black = Color(0xff010104);
+  static const white = Color(0xffFFFFFF);
+}
